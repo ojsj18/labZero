da459703b4b2d70ab7e2ec2a262d66a45e1662c0
ojsj18 <juhnior88@gmail.com>
2022-07-19 13:53:57 -0300

    doing

---
+++ ZeroFuncao.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "utils.h"
+#include "ZeroFuncao.h"
+
+double bisseccao (Polinomio p, double a, double b, double eps,
+	       int *it, double *raiz)
+{
+
+}
+
+
+double newtonRaphson (Polinomio p, double x0, double eps,
+		   int *it, double *raiz)
+{
+
+}
+
+
+double secante (Polinomio p, double x0, double x1, double eps,
+	     int *it, double *raiz)
+{
+
+}
+
+
+void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx)
+{
+
+}
+
+
+void calcPolinomio_lento(Polinomio p, double x, double *px, double *dpx)
+{
+
+}
---
+++ labZero.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "utils.h"
+#include "ZeroFuncao.h"
+
+int main ()
+{
+
+
+
+
+
+
+  return 0;
+}
+
